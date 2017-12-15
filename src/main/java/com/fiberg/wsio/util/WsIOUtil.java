ff3c45d0f4b5d62fa901a9b7f21f8271534c59c2
jmdsg <jose.mdsg@gmail.com>
2017-12-15 14:13:19 -0400

    Useless change

--- src/main/java/com/fiberg/wsio/util/WsIOUtil.java
+++ src/main/java/com/fiberg/wsio/util/WsIOUtil.java
@@ -13,6 +13,12 @@ public final class WsIOUtil {
 
 	private WsIOUtil() {  }
 
+
+
+
+
+
+	
 	public static final String NAME_SEPARATOR = ".";
 
 	public static String addSuffixOf(String name, String suffix, String separator) {
