b37ac383c141ed6c1b55892a210e071490ff0faf
jmdsg <jose.mdsg@gmail.com>
2017-12-15 21:44:20 -0400

    Wildcard line changes

--- src/main/java/com/fiberg/wsio/util/WsIOUtil.java
+++ src/main/java/com/fiberg/wsio/util/WsIOUtil.java
@@ -12,6 +12,7 @@ public final class WsIOUtil {
 
 	private WsIOUtil() {  }
 
+
 	public static final String NAME_SEPARATOR = ".";
 
 	public static String addSuffixOf(String name, String suffix, String separator) {
