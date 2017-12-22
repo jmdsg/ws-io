96e04f3a1b6a446d8904e509a7b9229bb13856c9
jmdsg <jose.mdsg@gmail.com>
2017-12-22 00:37:08 -0400

    Collection set to public

--- src/main/java/com/fiberg/wsio/processor/WsIOCollection.java
+++ src/main/java/com/fiberg/wsio/processor/WsIOCollection.java
@@ -5,11 +5,11 @@ import com.google.common.collect.ImmutableSet;
 
 import java.util.*;
 
-final class WsIOCollection {
+public final class WsIOCollection {
 
 	private WsIOCollection() {}
 
-	static final Map<String, String> IMPLEMENTATIONS = ImmutableMap.<String, String>builder()
+	public static final Map<String, String> IMPLEMENTATIONS = ImmutableMap.<String, String>builder()
 		.put(Collection.class.getCanonicalName(), ArrayList.class.getCanonicalName())
 		.put(List.class.getCanonicalName(), ArrayList.class.getCanonicalName())
 		.put(Queue.class.getCanonicalName(), PriorityQueue.class.getCanonicalName())
@@ -19,7 +19,7 @@ final class WsIOCollection {
 		.put(SortedMap.class.getCanonicalName(), TreeMap.class.getCanonicalName())
 		.build();
 
-	static final Set<String> COLLECTION_INTERFACES = ImmutableSet.<String>builder()
+	public static final Set<String> COLLECTION_INTERFACES = ImmutableSet.<String>builder()
 			.add(Collection.class.getCanonicalName())
 			.add(List.class.getCanonicalName())
 			.add(Queue.class.getCanonicalName())
@@ -27,12 +27,12 @@ final class WsIOCollection {
 			.add(SortedSet.class.getCanonicalName())
 			.build();
 
-	static final Set<String> MAP_INTERFACES = ImmutableSet.<String>builder()
+	public static final Set<String> MAP_INTERFACES = ImmutableSet.<String>builder()
 			.add(Map.class.getCanonicalName())
 			.add(SortedMap.class.getCanonicalName())
 			.build();
 
-	static final Set<String> COLLECTION_CLASSES = ImmutableSet.<String>builder()
+	public static final Set<String> COLLECTION_CLASSES = ImmutableSet.<String>builder()
 			.add(ArrayList.class.getCanonicalName())
 			.add(Vector.class.getCanonicalName())
 			.add(LinkedList.class.getCanonicalName())
@@ -42,24 +42,24 @@ final class WsIOCollection {
 			.add(TreeSet.class.getCanonicalName())
 			.build();
 
-	static final Set<String> MAP_CLASSES = ImmutableSet.<String>builder()
+	public static final Set<String> MAP_CLASSES = ImmutableSet.<String>builder()
 			.add(HashMap.class.getCanonicalName())
 			.add(LinkedHashMap.class.getCanonicalName())
 			.add(Hashtable.class.getCanonicalName())
 			.add(TreeMap.class.getCanonicalName())
 			.build();
 
-	static final Set<String> MAPS = ImmutableSet.<String>builder()
+	public static final Set<String> MAPS = ImmutableSet.<String>builder()
 			.addAll(MAP_INTERFACES)
 			.addAll(MAP_CLASSES)
 			.build();
 
-	static final Set<String> COLLECTIONS = ImmutableSet.<String>builder()
+	public static final Set<String> COLLECTIONS = ImmutableSet.<String>builder()
 			.addAll(COLLECTION_INTERFACES)
 			.addAll(COLLECTION_CLASSES)
 			.build();
 
-	static final Set<String> ALL = ImmutableSet.<String>builder()
+	public static final Set<String> ALL = ImmutableSet.<String>builder()
 			.addAll(MAPS)
 			.addAll(COLLECTIONS)
 			.build();
